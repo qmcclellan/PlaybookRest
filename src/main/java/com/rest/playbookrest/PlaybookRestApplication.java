f0c6854cefeaaa732fd7a7d4b7d5751d90409647
qmcclellan <qmcclellan@gmail.com>
2023-10-05 21:01:41 -0400

    first commit

---
+++ src/main/java/com/rest/playbookrest/PlaybookRestApplication.java
@@ -0,0 +1,13 @@
+package com.rest.playbookrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaybookRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlaybookRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rest/playbookrest/PlaybookRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.playbookrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaybookRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
