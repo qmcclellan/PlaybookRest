dda1d59e190abb8cdc61a69ca9a1981a46a95ff7
qmcclellan <qmcclellan@gmail.com>
2023-10-13 13:10:35 -0400

    Added controllers

---
+++ src/main/java/com/rest/playbookrest/Controller/CoachController.java
@@ -0,0 +1,11 @@
+package com.rest.playbookrest.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Coach")
+public class CoachController {
+
+
+}
---
+++ src/main/java/com/rest/playbookrest/Controller/PlayController.java
@@ -0,0 +1,9 @@
+package com.rest.playbookrest.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Play")
+public class PlayController {
+}
