1585b76623c56ca5dc6961236265c02bb10266d4
qmcclellan <qmcclellan@gmail.com>
2024-01-16 14:46:04 -0500

    Fixed Aop issues

--- src/main/java/com/rest/playbookrest/Aop/GlobalAop.java
+++ src/main/java/com/rest/playbookrest/Aop/GlobalAop.java
@@ -28,17 +28,17 @@ public class GlobalAop {
 
     }
 
-    @Pointcut("within(com.rest.playbookrest.Dao.*.*)"
-            + " || within(com.rest.playbookrest.Service.*.*)"
-            + " || within(com.rest.playbookrest.Controller.*.*)"
-            + " || within(com.rest.playbookrest.Dto.*.*)")
+    @Pointcut("within(com.rest.playbookrest.Dao..*)"
+            + " || within(com.rest.playbookrest.Service..*)"
+            + " || within(com.rest.playbookrest.Controller..*)"
+            + " || within(com.rest.playbookrest.Dto..*)")
     public void applicationPackagePointcut() {
       //  throw new UnsupportedOperationException();
     }
 
     @AfterThrowing(pointcut = "applicationPackagePointcut() && springBeanPointcut()", throwing = "e")
     public void logAfterThrowing(JoinPoint joinPoint, Throwable e) {
-        logger.error("Exception in {}.{}() with cause = {}", joinPoint.getSignature().getDeclaringTypeName(),
+        logger.error("==============>>>>>> Exception in {}.{}() with cause = {}", joinPoint.getSignature().getDeclaringTypeName(),
                 joinPoint.getSignature().getName(), e.getCause() != null ? e.getMessage() : "NULL");
     }
 
@@ -46,14 +46,12 @@ public class GlobalAop {
     public Object invoke(ProceedingJoinPoint joinPoint) throws Throwable {
         final String joinPoints = Arrays.toString(joinPoint.getArgs());
 
-        logger.info("==============>>>>>>"+joinPoint.getSignature().getClass()+"*******************");
-
         if (joinPoints != null) {
-            logger.info("Enter: {}.{}() with argument[s] = {}", joinPoint.getSignature().getDeclaringTypeName(),
+            logger.info("==============>>>>>> Enter: {}.{}() with argument[s] = {}", joinPoint.getSignature().getDeclaringTypeName(),
                     joinPoint.getSignature().getName(), joinPoints +"************");
         }
         final Object result = joinPoint.proceed();
-        logger.info("Exit: {}.{}() with result = {}", joinPoint.getSignature().getDeclaringTypeName(),
+        logger.info("==============>>>>>> Exit: {}.{}() with result = {}", joinPoint.getSignature().getDeclaringTypeName(),
                 joinPoint.getSignature().getName(), result);
         return result;
     }
